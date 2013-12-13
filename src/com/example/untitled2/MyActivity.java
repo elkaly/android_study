d75b669442c730c6b929e79682e027f064837259
elkaly <elkaly@naver.com>
2013-12-13 22:16:44 +0900

    화면열기 테스트

--- src/com/example/untitled2/MyActivity.java
+++ src/com/example/untitled2/MyActivity.java
@@ -2,16 +2,55 @@ package com.example.untitled2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.view.View;
 
-public class MyActivity extends Activity {
+import android.content.Intent;
+
+
+public class MyActivity extends Activity implements OnClickListener{
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.main);
+
+        Button testBtn = (Button)findViewById(R.id.startBtn);
+
+
+        testBtn.setOnClickListener(this);
+
+
+        /*
+        testBtn.setOnClickListener(new OnClickListener(){
+           @Override
+            public void onClick(View v){
+
+               Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+           }
+
+
+        });*/
+
     }
+
+    @Override
+    public void onClick(View v){
+        int sel = v.getId();
+        switch(sel){
+            case R.id.startBtn:
+
+                Intent intent = new Intent(MyActivity.this, SecondActivity.class);
+                startActivity(intent);
+                //Toast.makeText(this, "TEs2", Toast.LENGTH_SHORT).show();
+            break;
+
+
+        }
+    }
+
 }
---
+++ src/com/example/untitled2/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.untitled2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: elkaly
+ * Date: 2013. 12. 13.
+ * Time: 오후 9:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class SecondActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+
+    }
+}
