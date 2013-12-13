16fbd29f939cd7dde967c6d69dac4fa6d6ad52f4
elkaly <elkaly@naver.com>
2013-12-13 22:36:49 +0900

    Intent 테스트

--- src/com/example/untitled2/MyActivity.java
+++ src/com/example/untitled2/MyActivity.java
@@ -1,6 +1,7 @@
 package com.example.untitled2;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -44,8 +45,24 @@ public class MyActivity extends Activity implements OnClickListener{
         switch(sel){
             case R.id.startBtn:
 
-                Intent intent = new Intent(MyActivity.this, SecondActivity.class);
-                startActivity(intent);
+                //화면 띄우기
+                //Intent intent = new Intent(MyActivity.this, SecondActivity.class);
+                //startActivity(intent);
+
+                //전화걸기
+                //Intent intentCall = new Intent();
+                //intentCall.setAction(Intent.ACTION_DIAL);
+                //intentCall.setData(Uri.parse("tel:01090580695"));
+                //startActivity(intentCall);
+
+                //SMS보내기
+                Intent intentSms = new Intent();
+                intentSms.setAction(Intent.ACTION_VIEW);
+                intentSms.setData(Uri.parse("smsto:01090580695"));
+                intentSms.putExtra("sms_body", "Hello~!");
+                startActivity(intentSms);
+
+
                 //Toast.makeText(this, "TEs2", Toast.LENGTH_SHORT).show();
             break;
 
